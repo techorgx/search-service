61b423cee693e83deaf91db76d022b9144a7bddc
Aman Rajput <amrajput@expediagroup.com>
2024-03-05 20:40:55 +0530

    Renamed messages and request in proto file

--- grpc/src/main/kotlin/com/techorgx/api/grpc/SearchApi.kt
+++ grpc/src/main/kotlin/com/techorgx/api/grpc/SearchApi.kt
@@ -1,13 +1,13 @@
 package com.techorgx.api.grpc
 
-import com.techorgx.search.api.v1.PersonRequest
-import com.techorgx.search.api.v1.PersonResponse
 import com.techorgx.search.api.v1.SearchApiGrpcKt
+import com.techorgx.search.api.v1.SearchRequest
+import com.techorgx.search.api.v1.SearchResponse
 import org.lognet.springboot.grpc.GRpcService
 
 @GRpcService
 class SearchApi : SearchApiGrpcKt.SearchApiCoroutineImplBase() {
-    override suspend fun getPerson(request: PersonRequest): PersonResponse {
-        return super.getPerson(request)
+    override suspend fun searchAd(searchRequest: SearchRequest): SearchResponse {
+        return super.searchAd(searchRequest)
     }
 }
